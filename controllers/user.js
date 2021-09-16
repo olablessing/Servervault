e4007d14bc97c2b48613a90426820162db3e9527
oladipupo Blessing <psarmmiey@gmail.com>
2021-09-16 06:20:37 +0100

    update

--- controllers/user.js
+++ controllers/user.js
@@ -58,9 +58,9 @@ export const signup = async (req, res) => {
         name: `${firstName} ${lastName}`,
       });
 
-      const tgNames = await Usernames.create({
-        telegramusername,
-      });
+      // const tgNames = await Usernames.create({
+      //   telegramusername,
+      // });
 
       const token = jwt.sign({ email: result.email, id: result._id }, "gain", {
         expiresIn: "1h",
@@ -73,65 +73,65 @@ export const signup = async (req, res) => {
   }
 };
 
-export const resetPassword = async (req, res) => {
-  const { email, password, newpassword, newconfirmpassword } = req.body;
-  console.log(`${email} ${password} is logging in`);
-  try {
-    const result = await User.findOne({ email });
-    console.log(result);
-    if (!result) {
-      res.status(404).json({ message: "User does not exist." });
-    }
-    const isPasswordCorrect = await bcrypt.compare(password, result.password);
-    if (!isPasswordCorrect) {
-      res.status(404).json({ message: "invalid credentials" });
-    }
+// export const resetPassword = async (req, res) => {
+//   const { email, password, newpassword, newconfirmpassword } = req.body;
+//   console.log(`${email} ${password} is logging in`);
+//   try {
+//     const result = await User.findOne({ email });
+//     console.log(result);
+//     if (!result) {
+//       res.status(404).json({ message: "User does not exist." });
+//     }
+//     const isPasswordCorrect = await bcrypt.compare(password, result.password);
+//     if (!isPasswordCorrect) {
+//       res.status(404).json({ message: "invalid credentials" });
+//     }
 
-    if (newpassword !== newconfirmpassword) {
-      res.status(404).json({ message: "passwords dont match" });
-    }
-    const hashedPassword = await bcrypt.hash(newpassword, 12);
+//     if (newpassword !== newconfirmpassword) {
+//       res.status(404).json({ message: "passwords dont match" });
+//     }
+//     const hashedPassword = await bcrypt.hash(newpassword, 12);
 
-    await User.updateOne({ email }, { password: hashedPassword });
-    res.status(200).json({ result });
-  } catch (error) {
-    res.status(404).json({ message: "Something went wrong " + error });
-  }
-};
+//     await User.updateOne({ email }, { password: hashedPassword });
+//     res.status(200).json({ result });
+//   } catch (error) {
+//     res.status(404).json({ message: "Something went wrong " + error });
+//   }
+// };
 
-export const updateProfile = async (req, res) => {
-  const { name, email, telegramusername } = req.body;
-  try {
-    const result = await user.findOne({ email });
-    console.log(result);
-    if (!result) {
-      res.status(404).json({ message: "User Does Not Exist" });
-    }
+// export const updateProfile = async (req, res) => {
+//   const { name, email, telegramusername } = req.body;
+//   try {
+//     const result = await user.findOne({ email });
+//     console.log(result);
+//     if (!result) {
+//       res.status(404).json({ message: "User Does Not Exist" });
+//     }
 
-    await User.updateOne(
-      { email },
-      { name: name, telegramusername: telegramusername }
-    );
+//     await User.updateOne(
+//       { email },
+//       { name: name, telegramusername: telegramusername }
+//     );
 
-    res.status(200).json({ result });
-  } catch (error) {
-    res.status(404).json({ message: "Something went wrong " + error });
-  }
-};
+//     res.status(200).json({ result });
+//   } catch (error) {
+//     res.status(404).json({ message: "Something went wrong " + error });
+//   }
+// };
 
-export const deleteUser = async (req, res) => {
-  const { email } = req.body;
-  console.log(req.boy);
-  try {
-    const userdelete = await User.findOne({ email });
-    console.log(userdelete);
-    console.log({ message: "user found." });
-    if (!userdelete) {
-      return res.send({ message: "User does not exist." });
-    }
-    await User.deleteOne({ email });
-    res.status(200).json(userdelete);
-  } catch (error) {
-    res.send({ message: "Something went wrong " + error });
-  }
-};
+// export const deleteUser = async (req, res) => {
+//   const { email } = req.body;
+//   console.log(req.boy);
+//   try {
+//     const userdelete = await User.findOne({ email });
+//     console.log(userdelete);
+//     console.log({ message: "user found." });
+//     if (!userdelete) {
+//       return res.send({ message: "User does not exist." });
+//     }
+//     await User.deleteOne({ email });
+//     res.status(200).json(userdelete);
+//   } catch (error) {
+//     res.send({ message: "Something went wrong " + error });
+//   }
+// };
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { deleteUser, resetPassword, signin, signup, updateProfile } from "../controllers/user.js";
+import {  signin, signup,  } from "../controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
-router.post("/resetpassword", resetPassword);
-router.post("/updateprofile",updateProfile);
-router.post("/deleteUser", deleteUser);
+// router.post("/resetpassword", resetPassword);
+// router.post("/updateprofile",updateProfile);
+// router.post("/deleteUser", deleteUser);
 
 export default router;
